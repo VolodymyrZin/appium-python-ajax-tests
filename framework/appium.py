fe6c7a73070ac3fe2c4e7b8e00ad3f54a8717907
VolodymyrZin <vl.zinych@gmail.com>
2026-02-28 21:00:07 +0200

    changed appium changed driver

--- framework/appium.py
+++ framework/appium.py
@@ -1,17 +1,38 @@
 from appium.webdriver.appium_service import AppiumService
 
+import os
+from appium.webdriver.appium_service import AppiumService
 
 class Appium:
-    service = AppiumService()
-    HOST = '127.0.0.1'
-    PORT = '4723'
+    service = None
+    HOST = os.getenv("APPIUM_HOST", "http://localhost")
+    PORT = os.getenv("APPIUM_PORT", "4723")
 
     @classmethod
     def start(cls) -> None:
-        cls.service.start(
-            args=['-a', cls.HOST, '-p', cls.PORT, '--relaxed-security', '--allow-insecure', 'adb_shell']
-        )
+        # запуск Appium тільки локально (не у Docker)
+        if os.getenv("DOCKER", "0") == "1":
+            return
+        cls.service = AppiumService()
+        cls.service.start(args=['-a', cls.HOST, '-p', cls.PORT, '--relaxed-security', '--allow-insecure', 'adb_shell'])
 
     @classmethod
     def stop(cls) -> None:
+        if os.getenv("DOCKER", "0") == "1" or not cls.service:
+            return
         cls.service.stop()
+
+# class Appium:
+#     service = AppiumService()
+#     HOST = '127.0.0.1'
+#     PORT = '4723'
+#
+#     @classmethod
+#     def start(cls) -> None:
+#         cls.service.start(
+#             args=['-a', cls.HOST, '-p', cls.PORT, '--relaxed-security', '--allow-insecure', 'adb_shell']
+#         )
+#
+#     @classmethod
+#     def stop(cls) -> None:
+#         cls.service.stop()
--- framework/driver.py
+++ framework/driver.py
@@ -1,12 +1,9 @@
 from contextlib import suppress
-
 from appium.options.android import UiAutomator2Options
 from appium.webdriver import Remote
 from appium.webdriver.appium_connection import AppiumConnection
 from selenium.common.exceptions import WebDriverException
-
 from appium_python_ajax_tests.framework.appium import Appium
-
 
 class Driver:
     app_package = 'com.ajaxsystems'
@@ -18,7 +15,7 @@ class Driver:
 
     @classmethod
     def finish(cls) -> None:
-        cls.appium_instance.terminate_app(cls.app_package)
+        cls.terminate_app()
         cls.appium_instance.quit()
         cls.appium_instance = None
 
@@ -41,7 +38,6 @@ class Driver:
 
         if int(platform_version) >= 10:
             permissions.append('ACCESS_BACKGROUND_LOCATION')
-
         if int(platform_version) >= 13:
             permissions.append('POST_NOTIFICATIONS')
 
@@ -51,3 +47,57 @@ class Driver:
                     'mobile: shell',
                     {'command': 'pm grant', 'args': [f'{cls.app_package} android.permission.{permission}']}
                 )
+
+# from contextlib import suppress
+#
+# from appium.options.android import UiAutomator2Options
+# from appium.webdriver import Remote
+# from appium.webdriver.appium_connection import AppiumConnection
+# from selenium.common.exceptions import WebDriverException
+#
+# from appium_python_ajax_tests.framework.appium import Appium
+#
+#
+# class Driver:
+#     app_package = 'com.ajaxsystems'
+#     appium_instance = None
+#
+#     @classmethod
+#     def start(cls, options: UiAutomator2Options) -> None:
+#         cls.appium_instance = Remote(AppiumConnection(f'{Appium.HOST}:{Appium.PORT}'), options=options)
+#
+#     @classmethod
+#     def finish(cls) -> None:
+#         cls.appium_instance.terminate_app(cls.app_package)
+#         cls.appium_instance.quit()
+#         cls.appium_instance = None
+#
+#     @classmethod
+#     def launch_app(cls) -> None:
+#         cls.appium_instance.activate_app(cls.app_package)
+#
+#     @classmethod
+#     def terminate_app(cls) -> None:
+#         with suppress(WebDriverException):
+#             cls.appium_instance.terminate_app(cls.app_package)
+#
+#     @classmethod
+#     def grant_application_permissions(cls) -> None:
+#         permissions = [
+#             'ACCESS_FINE_LOCATION', 'ACCESS_COARSE_LOCATION', 'READ_EXTERNAL_STORAGE',
+#             'WRITE_EXTERNAL_STORAGE', 'CAMERA', 'READ_CONTACTS'
+#         ]
+#         platform_version = cls.appium_instance.capabilities['platformVersion']
+#
+#         if int(platform_version) >= 10:
+#             permissions.append('ACCESS_BACKGROUND_LOCATION')
+#
+#         if int(platform_version) >= 13:
+#             permissions.append('POST_NOTIFICATIONS')
+#
+#         for permission in permissions:
+#             with suppress(WebDriverException):
+#                 cls.appium_instance.execute_script(
+#                     'mobile: shell',
+#                     {'command': 'pm grant', 'args': [f'{cls.app_package} android.permission.{permission}']}
+#                 )
