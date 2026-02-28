38d74524785f02ea04c0469edfdec98ec191c70d
VolodymyrZin <vl.zinych@gmail.com>
2026-02-28 21:38:36 +0200

    changed android_utils.py

--- android_utils.py
+++ android_utils.py
@@ -1,14 +1,11 @@
+import os
+from appium.options.android import UiAutomator2Options
 import subprocess
 
-from appium.options.android import UiAutomator2Options
-
-
-adb_output = subprocess.getoutput('adb devices')
-if not adb_output or len(adb_output.splitlines()) == 1:
-    raise EnvironmentError('No Android device found')
-else:
-    udid = adb_output.splitlines()[1].split()[0]
-
+# беремо UDID пристрою з змінної оточення
+udid = os.getenv("DEVICE_UDID")
+if not udid:
+    raise EnvironmentError('No DEVICE_UDID provided. Set DEVICE_UDID=emulator-5554 (або інший UDID вашого пристрою)')
 
 def get_driver_options() -> UiAutomator2Options:
     options = UiAutomator2Options()
@@ -19,9 +16,37 @@ def get_driver_options() -> UiAutomator2Options:
     options.disable_window_animation = True
     return options
 
-
 def reset_app(package) -> None:
+    # Використовуємо ADB через переданий UDID
     subprocess.run(
         ['adb', '-s', udid, 'shell', 'pm', 'clear', package],
         stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL
     )
+
+# import subprocess
+#
+# from appium.options.android import UiAutomator2Options
+#
+#
+# adb_output = subprocess.getoutput('adb devices')
+# if not adb_output or len(adb_output.splitlines()) == 1:
+#     raise EnvironmentError('No Android device found')
+# else:
+#     udid = adb_output.splitlines()[1].split()[0]
+#
+#
+# def get_driver_options() -> UiAutomator2Options:
+#     options = UiAutomator2Options()
+#     options.no_reset = True
+#     options.udid = udid
+#     options.clear_device_logs_on_start = True
+#     options.auto_grant_permissions = True
+#     options.disable_window_animation = True
+#     return options
+#
+#
+# def reset_app(package) -> None:
+#     subprocess.run(
+#         ['adb', '-s', udid, 'shell', 'pm', 'clear', package],
+#         stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL
+#     )
